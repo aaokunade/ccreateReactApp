9e913d4860365da3dcd1a968c6647776c2967060
aaokunade <67062110+aaokunade@users.noreply.github.com>
2021-03-21 21:40:26 +0000

    catchPokemon component added to the page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import {Logo, BestPokemon,CaughtPokemon} from "./Pokemon";
 
 const abilities = ["Anticipation", "Adaptability", "Run-Away"];
 const date = new Date().toLocaleDateString();
+const logWhenClicked = () => {
+  console.log("hi from logo");
+}
+
 function App(){
     return  (<div>
-    <Logo appName = "Aaishah's Pokedex"/>
+    <Logo appName = "Aaishah's Pokedex" handleClick={logWhenClicked}/>
     <BestPokemon abilities = {abilities}/>
     <CaughtPokemon date = {date}/>
   </div>)  
